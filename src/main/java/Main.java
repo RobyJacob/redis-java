11343d92765db3013a19fe94095de2f2be572575
roby <robyjacob1998@gmail.com>
2024-03-25 14:48:07 +0530

    read from stream continuously

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,46 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.Buffer;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
-      public static void main(String[] args){
-    //       You can use print statements as follows for debugging, they'll be visible when running tests.
-            System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        //       You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
-    //        Uncomment this block to pass the first stage
-            ServerSocket serverSocket = null;
-            Socket clientSocket = null;
-            BufferedReader reader = null;
-            PrintWriter writer = null;
-            int port = 6379;
-            try {
-                serverSocket = new ServerSocket(port);
-                // Since the tester restarts your program quite often, setting SO_REUSEADDR
-                // ensures that we don't run into 'Address already in use' errors
-                serverSocket.setReuseAddress(true);
-                // Wait for connection from client.
-                clientSocket = serverSocket.accept();
+        //        Uncomment this block to pass the first stage
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        BufferedReader reader = null;
+        int port = 6379;
+        try {
+            serverSocket = new ServerSocket(port);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+            clientSocket = serverSocket.accept();
 
+            reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            while (reader.readLine() != null) {
                 respondToClient(clientSocket);
+            } 
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
             } catch (IOException e) {
                 System.out.println("IOException: " + e.getMessage());
-            } finally {
-                try {
-                    if (clientSocket != null) {
-                      clientSocket.close();
-                    }
-                } catch (IOException e) {
-                    System.out.println("IOException: " + e.getMessage());
-                }
             }
-      }
+        }
+    }
 
     private static void respondToClient(Socket clientSocket) throws IOException {
-          String response = "+PONG\r\n";
-          clientSocket.getOutputStream().write(response.getBytes(StandardCharsets.UTF_8));
+        String response = "+PONG\r\n";
+        clientSocket.getOutputStream().write(response.getBytes(StandardCharsets.UTF_8));
     }
 }
